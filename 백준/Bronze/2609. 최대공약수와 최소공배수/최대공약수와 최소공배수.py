c91d24cd606e1ebbd4d595d8df30d98e3c0635ff
giyeon <165770597+giyeon-dev@users.noreply.github.com>
2024-07-14 13:39:44 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    ans = (a * b) // gcd(a, b)
+    return ans
+
+print(gcd(a, b))
+print(lcm(a, b))
