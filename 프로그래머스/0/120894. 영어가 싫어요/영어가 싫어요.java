c511bd2532cb418ca3ed606a140d0762b1177257
giyeon <165770597+giyeon-dev@users.noreply.github.com>
2024-08-13 13:09:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String[] eng = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for (int i = 0; i < eng.length; i++){
+            numbers = numbers.replace(eng[i], String.valueOf(i));
+        }
+        
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
+}
