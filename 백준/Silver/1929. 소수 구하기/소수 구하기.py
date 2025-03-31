e5e414492da2b3222d784c062ec2a59445370501
giyeon <165770597+giyeon-dev@users.noreply.github.com>
2025-03-31 20:45:17 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,17 +1,17 @@
-import math
+
 M, N = map(int, input().split())
 
+def sieve_of_eratosthenes(m, n):
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False
 
-def isPrime(num):
-    if num == 1:
-        return False
+    for i in range(2, int(n**0.5) + 1):
+        if prime[i]:
+            for j in range(i*i, n+1, i):
+                prime[j] = False
 
-    for n in range(2, int(math.sqrt(num)) + 1):
-        if num % n == 0:
-            return False
+    for i in range(m, n+1):
+        if prime[i]:
+            print(i)
 
-    return True
-
-for n in range(M, N + 1):
-    if isPrime(n):
-        print(n)
+sieve_of_eratosthenes(M,N)
