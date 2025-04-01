3856e68cd842ce94f4f9f3305efaff44986e9e58
giyeon <165770597+giyeon-dev@users.noreply.github.com>
2025-04-01 16:34:43 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n <= 3:
+    print(1)
+
+else:
+    dp = [0] * (n + 1)
+    dp[1] = dp[2] = dp[3] = 1
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i-1] + dp[i-3]
+
+    print(dp[n])
