7241f908f07848c62f2b13b447d46186f1f4121d
giyeon <165770597+giyeon-dev@users.noreply.github.com>
2025-04-04 23:14:42 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -4,8 +4,8 @@ nums = list(map(int, input().split()))
 temp_sum = sum(nums[:K])
 max_sum = temp_sum
 
-for i in range(K, N):
-    temp_sum = temp_sum - nums[i - K] + nums[i]
+for i in range(N-K):
+    temp_sum += nums[K + i] - nums[i]
     max_sum = max(max_sum, temp_sum)
 
 print(max_sum)
